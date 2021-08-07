f3ef72e2dbb5476b49027804fffb5ae2f3d06263
Carlos Moreno Quintanar <carlos.sigma66@gmail.com>
2021-08-07 17:45:33 -0500

    First Commit

---
+++ app/src/main/java/cmoreno/example/coderswag/MainActivity.kt
@@ -0,0 +1,13 @@
+package cmoreno.example.coderswag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
